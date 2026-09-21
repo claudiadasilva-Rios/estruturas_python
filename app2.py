56638f6a5579593c4d0aa9e060539135f2aab027
claudiadasilva-Rios <claudiadasilvarios@gmail.com>
2026-09-21 12:52:53 -0300

    app1

---
+++ app2.py
@@ -0,0 +1,19 @@
+print()
+
+name = input ("Qual seu nome? ")
+
+age = input  ("Quantos anos você tem? ")
+
+print(type(name))
+print(type(age))
+
+age = int(age)
+
+older = int(age) + 10
+
+print()
+
+print = (f"{name} terá {older} daqui a 10 anos.")
+
+
+
